7ea3b6d78149ade23a087e976edaeb0dbfe20405
Rahul <Rahul.Jaiswal@bentley.com>
2024-10-09 17:51:32 +0530

    commit

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,9 @@
+import Link from "next/link";
 
 export default function Home() {
     console.log('test11111111111111');
-    return <h1>test</h1>
+    return <>
+          <Link href="/">Root</Link>
+          <h1>test</h1>
+          </>
 }
