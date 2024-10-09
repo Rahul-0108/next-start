cd30c3240c454f67a33f59345783610d3cd0dabe
Rahul <Rahul.Jaiswal@bentley.com>
2024-10-09 16:36:30 +0530

    commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,5 @@
+
+export default function Home() {
+    console.log('test11111111111111');
+    return <h1>test</h1>
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className={styles.page}>
+      <Link href="/about">About</Link>
+      <Link href="/test">About</Link>
       <main className={styles.main}>
         <Image
           className={styles.logo}
